6a583c4ed19da8e3419855656a5a0f67551c0910
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-03 18:43:17 -0800

    removed import of Pokecard, now importing Pokedex

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
-import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokecard
-          id={4}
-          name={"Charmander"}
-          type={"fire"}
-          base_experience={62}
-        />
+        <Pokedex />
       </div>
     );
   }
