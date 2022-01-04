c3b0d16c9ef780b7c93252fb7990eb2f60222521
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-04 14:08:35 -0800

    commented out previous methods of adjusting pokemon id to make use of nicer images api

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,22 +1,28 @@
 import React from "react";
-import './Pokecard.css';
+import "./Pokecard.css";
 
 export default function Pokecard({ id, name, type, base_experience }) {
   const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
-  let modifiedId = id.toString();
-  if (modifiedId.length === 1) {
-    modifiedId = "00" + modifiedId;
-  } else if (modifiedId.length === 2) {
-    modifiedId = "0" + modifiedId;
-  }
-  let imgSrc = `${POKE_API}${modifiedId}.png`;
+  //   let modifiedId = id.toString();
+  //   if (modifiedId.length === 1) {
+  //     modifiedId = "00" + modifiedId;
+  //   } else if (modifiedId.length === 2) {
+  //     modifiedId = "0" + modifiedId;
+  //   }
+  //   while (modifiedId.length < 3) {
+  //     modifiedId = "0" + modifiedId;
+  //   }
+
+  let padToThree = (number) =>
+    number <= 999 ? `00${number}`.slice(-3) : number;
+  let imgSrc = `${POKE_API}${padToThree(id)}.png`;
   return (
     <div className="Pokecard">
-      <h2>{name}</h2>
+      <h2 className="Pokecard-title">{name}</h2>
       <img src={imgSrc} alt={name} />
-      <div>Type: {type}</div>
-      <div>EXP: {base_experience}</div>
+      <div className="Pokecard-data">Type: {type}</div>
+      <div className="Pokecard-data">EXP: {base_experience}</div>
     </div>
   );
 }
